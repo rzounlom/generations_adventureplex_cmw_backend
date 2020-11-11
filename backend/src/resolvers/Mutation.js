48b4c9aaf2fffd7204181ae8fde5b200eb527f56
rzounlome <rzounlome@harmonyhit.com>
2020-11-11 15:00:31 -0500

    updated editUser mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 const Mutation = {
+  //user mutations
     createUser : async (parent, args,{models:{UserModel}}, info) => {
       const {email, password, role, name} = args.data
            //check if email taken
@@ -13,10 +14,11 @@ const Mutation = {
     const user = await UserModel.create({ name, password, email, role });
     return user;
     },
-    updateUser: async (parent, args, {models:{UserModel}}, info) => {
-        const {id, data} = args
+    updateUser: async (parent, {id, data}, {models:{UserModel}}, info) => {
+      const {email, role, name, password} = data
         //see if user exists
-        const user = UserModel.findById({_id: id})
+        const user = await UserModel.findById({_id: id})
+
         //throw error if user does not exists
         if(!user) {
             throw new Error("User not found")
@@ -24,19 +26,44 @@ const Mutation = {
 
         //check data
         const emailTaken = await UserModel.findOne({email}).exec();
-      if (emailTaken) {
-        throw new Error("Email taken");
-      }
 
-      user.email = data.email;
+        if (emailTaken) {
+          user.email = user.email;
+        } else {
+          user.email = email;
+        }
 
-      if (typeof data.name === "string") {
-        user.name = data.name;
-      }
-      await user.save()
+        if (name && typeof name === "string") {
+          user.name = name;
+        } else {
+          user.name = user.name
+        }
 
-      return user;
+        if (password && typeof password === "string") {
+          user.password = password;
+        } else {
+          user.password = user.password
+        }
+
+        if (role) {
+          user.role = role;
+        } else {
+          user.role = user.role
+        }
+
+        try {
+          await user.save()
+          console.log("User Updated: " + user)
+          return user
+        } catch (error) {
+          if(error) {
+            console.log(error)
+            throw new Error("User Not Saved")
+          }
+        }
     },
+
+    //client mutations
     createClient: async (parent, args, {models:{ClientModel}}, info) => {
         const {name, phone} = args.data
         const nameTaken = await ClientModel.findOne({name}).exec()
